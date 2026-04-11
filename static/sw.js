ea27b199365cf7a5aa62baaceb2b75871220de44
sudeep291 <sudeep291@users.noreply.github.com>
2026-04-11 15:42:48 +0530

    style(ui): crop and adjust update stock container and background focus

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sv-footwear-v14'; // Bump to force update
+const CACHE_NAME = 'sv-footwear-v15'; // Bump to force update
 const ASSETS_TO_CACHE = [
   '/',
   '/static/css/style.css',
